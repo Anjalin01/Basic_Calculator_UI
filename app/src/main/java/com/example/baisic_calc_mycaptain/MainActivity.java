c2f9d596ded2e93a2005fe4107fed0a43fc539bf
Anjalin <anjalin2001@gmail.com>
2022-02-27 21:53:36 +0400

    Basic Calculator UI

---
+++ app/src/main/java/com/example/baisic_calc_mycaptain/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.baisic_calc_mycaptain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
